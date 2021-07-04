9a7f0fecd2b3efed6e9ae4ec54ef92fe19c84562
joelgrod <joelgrod@gmail.com>
2021-07-04 21:32:34 +0100

    email config and domain added

--- server/email-config.ts
+++ server/email-config.ts
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 export const transport = nodemailer.createTransport({
-    service: 'Hotmail',
+    service: String(process.env.EMAIL_SERVICE),
     auth: {
         user: String(process.env.EMAIL_USER),
         pass: String(process.env.EMAIL_PASS)
